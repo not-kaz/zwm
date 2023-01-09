4ef1c26535ddea399d78fc23210ab6f594b277f6
Kaz <87723604+not-kaz@users.noreply.github.com>
2023-01-09 12:43:56 +0100

    Added config values for window apperance

--- config.h
+++ config.h
@@ -5,6 +5,11 @@
 #define MOD_KEY   XCB_MOD_MASK_4
 #define MOD_SHIFT XCB_MOD_MASK_SHIFT
 
+/* Window appearance */ 
+#define BORDER_WIDTH 1
+#define BORDER_COLOR_FOCUSED 0xffffff
+#define BORDER_COLOR_UNFOCUSED 0xd0d0d0
+
 /* Key bindings */
 static const struct key keys[] = {
 	/* MOD	     KEY       FUNC     ARG */
